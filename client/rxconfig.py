5e9c560e9deb53f8df737af7ef82816e46a52bf0
schnatr <schnatr@gmail.com>
2025-11-11 02:31:24 +0700

    feat : docker compose reflex

--- client/rxconfig.py
+++ client/rxconfig.py
@@ -2,6 +2,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="my_app_name",
+    frontend_packages=[
+        "@react-three/fiber@9.3.0",
+        "@react-three/drei@10.7.6",
+        "three@0.179.1",
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
